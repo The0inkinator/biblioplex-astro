43ca9a829f40ce28dd42a8bfeee45ec070fbf08e
TheOinkinator <theoinkinator@gmail.com>
2023-03-27 23:28:37 -0700

    implement test mapping

---
+++ src/scripts/lists.js
@@ -0,0 +1,13 @@
+export const mainPageGrouping = [
+  { title: "", subtitle: "", imgNum: "", link: "" },
+  { title: "", subtitle: "", imgNum: "", link: "" },
+  { title: "", subtitle: "", imgNum: "", link: "" },
+  { title: "", subtitle: "", imgNum: "", link: "" },
+  { title: "", subtitle: "", imgNum: "", link: "" },
+];
+
+export const BgColors = [
+  "https://cards.scryfall.io/art_crop/front/b/8/b8d58c2d-2bff-4d25-a023-0a6342167f5f.jpg?1674142441",
+  "black",
+  "red",
+];
