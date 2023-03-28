472f79655f41402d3d8ce70309ef5f71f2b790d5
TheOinkinator <theoinkinator@gmail.com>
2023-03-28 23:41:58 +0000

    Establish TestNavCardMapping

--- src/scripts/lists.js
+++ src/scripts/lists.js
@@ -4,45 +4,12 @@ export const BgColors = [
   "red",
 ];
 
-export const mainPageGrouping = [
-  {
-    title: "Lands",
-    subtitle: "Lands subtitle",
-    imgSrc:
-      "https://cards.scryfall.io/art_crop/front/7/f/7f8c9ac8-0741-4261-b43b-d745839e11f5.jpg?1562925071",
-    link: "/",
-  },
-  {
-    title: "Creatures",
-    subtitle: "Creatures Subtitle",
-    imgSrc:
-      "https://cards.scryfall.io/art_crop/front/d/7/d7ce9104-0ad3-4d3d-bb2c-c456c25030f6.jpg?1679497505",
-    link: "/",
-  },
-  {
-    title: "Spells",
-    subtitle: "Spells subtitle",
-    imgSrc:
-      "https://cards.scryfall.io/art_crop/front/2/0/20b5fe42-929c-406d-9377-40b49f9d2e2c.jpg?1673149167",
-    link: "/",
-  },
-  {
-    title: "Archetypes",
-    subtitle: "Archetypes subtitle",
-    imgSrc:
-      "https://cards.scryfall.io/art_crop/front/1/7/17416926-168b-49b3-9231-acbb8f8a1d13.jpg?1557577188",
-    link: "/",
-  },
-  {
-    title: "Colors",
-    subtitle: "Colors subtitle",
-    imgSrc:
-      "https://cards.scryfall.io/art_crop/front/5/0/504a69eb-3c2d-4bb1-b117-252b15acf0c2.jpg?1593096534",
-    link: "Archives",
-  },
+export const testGrouping = [
+  { name: "Item 1", color: "red" },
+  { name: "Item 2", color: "blue" },
 ];
 
-export const mainPageGrouping1 = [
+export const mainPageGrouping = [
   {
     title: "Lands",
     subtitle: "Lands subtitle",
