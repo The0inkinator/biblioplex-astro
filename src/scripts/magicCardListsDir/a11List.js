d58a2655a2e0655e6fcb38dcc2d7180367ee3586
TheOinkinator <theoinkinator@gmail.com>
2023-03-30 19:55:02 -0700

    finalize .js list driven magiccardgrid

--- src/scripts/magicCardListsDir/a11List.js
+++ src/scripts/magicCardListsDir/a11List.js
@@ -2,26 +2,31 @@ export const a11ListRaw = [
   {
     title: "Mulldrifter",
     imgSrc:
-      "https://cards.scryfall.io/art_crop/front/e/0/e0fed1e5-fcbd-4597-91b5-ba809571573b.jpg?1673147377",
+      "https://cards.scryfall.io/large/front/7/8/78fb8900-d28d-4e33-96a7-66fcbc117adf.jpg?1634348984",
+    link: "https://scryfall.com/",
   },
   {
     title: "Mulldrifter",
     imgSrc:
-      "https://cards.scryfall.io/art_crop/front/e/0/e0fed1e5-fcbd-4597-91b5-ba809571573b.jpg?1673147377",
+      "https://cards.scryfall.io/large/front/e/0/e0fed1e5-fcbd-4597-91b5-ba809571573b.jpg?1673147377",
+    link: "https://scryfall.com/",
   },
   {
     title: "Mulldrifter",
     imgSrc:
-      "https://cards.scryfall.io/art_crop/front/e/0/e0fed1e5-fcbd-4597-91b5-ba809571573b.jpg?1673147377",
+      "https://cards.scryfall.io/large/front/e/0/e0fed1e5-fcbd-4597-91b5-ba809571573b.jpg?1673147377",
+    link: "https://scryfall.com/",
   },
   {
     title: "Mulldrifter",
     imgSrc:
-      "https://cards.scryfall.io/art_crop/front/e/0/e0fed1e5-fcbd-4597-91b5-ba809571573b.jpg?1673147377",
+      "https://cards.scryfall.io/large/front/e/0/e0fed1e5-fcbd-4597-91b5-ba809571573b.jpg?1673147377",
+    link: "https://scryfall.com/",
   },
   {
     title: "Mulldrifter",
     imgSrc:
-      "https://cards.scryfall.io/art_crop/front/e/0/e0fed1e5-fcbd-4597-91b5-ba809571573b.jpg?1673147377",
+      "https://cards.scryfall.io/large/front/e/0/e0fed1e5-fcbd-4597-91b5-ba809571573b.jpg?1673147377",
+    link: "https://scryfall.com/",
   },
 ];
